2c16d68fe4793e64fe10aaca60887a6edaabee1b
taiju1780 <ancient.maou@gmail.com>
2019-10-09 15:03:33 +0900

    カメラの回転制御したい

---
+++ BattleRoyale/Assets/Scripts/Move.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public float speed = 3.0f;
+
+   
+    private void Start()
+    {
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("w"))
+        {
+            transform.position += transform.forward * speed * Time.deltaTime;
+        }
+        if (Input.GetKey("s"))
+        {
+            transform.position -= transform.forward * speed * Time.deltaTime;
+        }
+        if (Input.GetKey("d"))
+        {
+            transform.position += transform.right * speed * Time.deltaTime;
+        }
+        if (Input.GetKey("a"))
+        {
+            transform.position -= transform.right * speed * Time.deltaTime;
+        }
+    }
+}
---
+++ BattleRoyale/Assets/Scripts/cameraControll.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraControll : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset;
+
+    public Transform verRot;
+    public Transform horRot;
+    // Start is called before the first frame update
+    void Start()
+    {verRot = transform.parent;
+        horRot = GetComponent<Transform>();
+        offset = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset; 
+
+        float X_Rotation = Input.GetAxis("Mouse X");
+        float Y_Rotation = Input.GetAxis("Mouse Y");
+
+        if(horRot.transform.rotation.eulerAngles.y > 75)
+        {
+            Y_Rotation = 75;
+        } 
+        if(horRot.transform.rotation.eulerAngles.y < -75)
+        {
+            Y_Rotation = -75;
+        }
+        verRot.transform.Rotate(0, -X_Rotation * 2, 0);
+        horRot.transform.Rotate(-Y_Rotation * 2, 0, 0);
+    }
+}
