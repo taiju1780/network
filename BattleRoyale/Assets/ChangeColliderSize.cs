4bf9dbfdbb920df7303bd93d9f0344465bce8031
tmsao2 <masaki.takasu0727@gmail.com>
2019-10-15 13:09:46 +0900

    エリア収縮プログラム

---
+++ BattleRoyale/Assets/ChangeColliderSize.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColliderSize : MonoBehaviour
+{
+    CapsuleCollider area;
+    float rad;
+    public float timeOut;
+    private float timeElapsed;
+
+    bool timeflag;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        area = GetComponent<CapsuleCollider>();
+        timeflag = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeElapsed += Time.deltaTime;
+        if (timeflag == true)
+        {
+            area.radius -= 0.01f;
+
+        }
+        if (timeElapsed >= timeOut)
+        {
+            timeElapsed = 0.0f;
+            timeflag = !timeflag;
+        }
+    }
+}
+
